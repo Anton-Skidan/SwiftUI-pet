65322da78b14a1a19fa6647149eeedc225370938
Антон Скидан <AnASkidan@rnd.beeline.ru>
2023-01-24 21:27:52 +0300

    main
    main

---
+++ SwiftUI-pet/SwiftUI-pet/SwiftUI_petApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_petApp.swift
+//  SwiftUI-pet
+//
+//  Created by Антон Скидан on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_petApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
